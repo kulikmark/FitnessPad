1cfa05f0a3c4f281f47a2a85227d4d742a507a76
Mark Kulik <kulik_mark@mail.ru>
2022-03-19 13:38:17 +0300

    Initial Commit

---
+++ FitnessPad/FitnessPadApp.swift
@@ -0,0 +1,20 @@
+//
+//  FitnessPadApp.swift
+//  FitnessPad
+//
+//  Created by Марк Кулик on 19.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FitnessPadApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
